be66254a9754a7372eaa856b30d2936b262ddcd6
Zuparo1 <jacobh@hiof.no>
2023-10-08 12:56:56 +0200

    Testing repo

--- app/src/main/java/com/example/quizaplication/MainActivity.kt
+++ app/src/main/java/com/example/quizaplication/MainActivity.kt
@@ -31,10 +31,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
