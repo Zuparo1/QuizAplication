c42059c9a158cdcaf2f5d89fdd35acec2897d9b2
Zuparo1 <jacobh@hiof.no>
2023-10-08 13:15:01 +0200

    Creating small test Home_Screen

--- app/src/main/java/com/example/quizaplication/MainActivity.kt
+++ app/src/main/java/com/example/quizaplication/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.quizaplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quizaplication.ui.theme.QuizAplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color =  MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   HomeScreen({})
                 }
             }
         }
@@ -30,13 +41,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun HomeScreen(onQuizStartClick: () -> Unit
+) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to Quiz",
+            style = TextStyle(
+                fontSize = 24.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            ),
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(onClick = {onQuizStartClick()},
+            modifier = Modifier.padding(bottom = 16.dp)
+        ) {
+            Text(text = "Click to Start")
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     QuizAplicationTheme {
-        Greeting("Android")
+        HomeScreen({})
+
     }
 }
