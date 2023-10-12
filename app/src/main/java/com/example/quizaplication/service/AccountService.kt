35ccff002d1c11369b7e39660261093efef104b7
Jørgen <jorgenba@hiof.no>
2023-10-12 22:27:59 +0200

    Made functionality for creating creating an account and signing out

---
+++ app/src/main/java/com/example/quizaplication/service/AccountService.kt
@@ -0,0 +1,15 @@
+package com.example.quizaplication.service
+
+import com.example.quizaplication.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface AccountService {
+    val currentUserId: String
+    val hasUser: Boolean
+    val currentUser: Flow<User>
+
+    suspend fun createAnonymousAccount()
+    suspend fun authenticate(email: String, password: String, onResult: (Throwable?) -> Unit)
+    suspend fun linkAccount(email: String, password: String, onResult: (Throwable?) -> Unit)
+    suspend fun signOut()
+}
